36c605f05f45e3e950b33918511198ef8c05e5db
Zsombor Lehoczky <zslehoczky@users.noreply.github.com>
2025-10-02 18:49:32 +0200

    Make inheritance private

--- define/c_string_view.hpp
+++ define/c_string_view.hpp
@@ -4,14 +4,27 @@
 
 namespace final
 {
-    class c_string_view : public std::string_view
+    class c_string_view : private std::string_view
     {
     public:
         explicit c_string_view(const char *str = "") : std::string_view{str} {}
 
+        c_string_view(const c_string_view &other) = default;
+        c_string_view &operator=(const c_string_view &other) = default;
+
+        c_string_view(c_string_view &&other) = default;
+        c_string_view &operator=(c_string_view &&other) = default;
+
+        ~c_string_view() = default;
+
         [[nodiscard]] const char *c_str() const
         {
             return data();
+        }
+
+        const std::string_view &as_string_view() const
+        {
+            return *this;
         }
     };
 }
--- test/test_c_string_view.cpp
+++ test/test_c_string_view.cpp
@@ -1,6 +1,7 @@
 #include <c_string_view.hpp>
 
 #include <cassert>
+#include <string>
 
 using namespace final;
 
@@ -8,7 +9,11 @@ int main()
 {
     constexpr auto literal = "aa";
 
-    c_string_view val(literal);
+    c_string_view val{literal};
 
     assert(val.c_str() == literal);
+
+    std::string from_val{val.as_string_view()};
+
+    assert(from_val == literal);
 }
